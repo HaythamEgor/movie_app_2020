ea5447cb701d35bb2b1b0b93bc66688792214fc3
HaythamEgor <wisedain@gmail.com>
2020-09-18 16:21:07 +0900

    Edit 0918

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
+function Food({ fav }) {
+  return (
+    <h3>I love { fav }</h3>
+  );
+}
+
 function App() {
-  // return <div className="App" />;
-    return <div>Hello!!</div>;
+    return (
+    <div>
+      <h1>Hello!!</h1>
+      <Food fav="김치" />
+      <Food fav="스파게티" />
+      <Food fav="쭈꾸미" />
+      <Food fav="라면" />
+    </div>
+    );
 }
 
 export default App;
