5705cd57f6b0e93c1b55577ef60ad6725ecda006
HaythamEgor <wisedain@gmail.com>
2020-11-20 16:45:36 +0900

    Updates

---
+++ precache-manifest.3fca1b760f258bbc885c30d7381b85a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40df3f8ac86361dbaabbae30a1b1a425",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "3c0b9ccfb6520cbd7b25",
+    "url": "/movie_app_2020/static/css/main.6f158af0.chunk.css"
+  },
+  {
+    "revision": "880daf7d8848e66cb7a5",
+    "url": "/movie_app_2020/static/js/2.00c25197.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.00c25197.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c0b9ccfb6520cbd7b25",
+    "url": "/movie_app_2020/static/js/main.6d7be004.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
