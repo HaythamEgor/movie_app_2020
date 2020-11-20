27b55b0329754b36726f176938e1a551cb170750
HaythamEgor <wisedain@gmail.com>
2020-11-20 16:38:47 +0900

    1120 fix

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,33 +1,37 @@
 import React from 'react';
-
+import './Detail.css';
 class Detail extends React.Component {
-    componentDidMount() {
-        const { location, history } = this.props;
 
-        if (location.state === undefined) {
+  
+
+    componentDidMount() {
+        const {location,history} = this.props;
+
+        if(location.state === undefined){
             history.push('/');
         }
     }
 
     render() {
         const { location } = this.props;
-        if (location.state) {
-            return <div className="movie">                
-                       <img src = {location.state.poster}/>
-                       <span className="movie_data">
-                       <h1 className="movie_title">{location.state.title}</h1>
-                       <h5 className="movie_year">{location.state.year}</h5>
-                       <h5 className="movie_rating">★{location.state.rating}/10.0</h5>
-                       <h5 className="movie_genres">{location.state.genres}</h5>
-                       <h3 className="movie_summary">{location.state.summary}</h3>
-                       </span>
-                   </div>
-                   
+        
+        if(location.state){
+        return <div className="detail">
+        <img src ={location.state.poster}></img>
+            <span className="movie_data">
+                <h1>{location.state.title}</h1>
+                <h5>{location.state.year}</h5>
+                <h5>{location.state.genres}</h5>
+                <h4>★{location.state.rating}/10.0</h4>
+                <h3>{location.state.summary}</h3>
+            </span>
+        </div>
+        } else {
+            return null
         }
-        else {
-            return null;
-        }
+        
     }
+    
 }
 
 export default Detail;
